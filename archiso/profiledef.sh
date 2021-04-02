658fa0f42f663b74c234f24f174c37bc5c1ec899
Erik Dubois <erik.dubois@gmail.com>
2021-04-02 15:38:05 +0200

    archiso 52-1

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -3,16 +3,15 @@
 
 iso_name="archlinux"
 iso_label="ARCH_$(date +%Y%m)"
-iso_publisher="Arch Linux <https://www.archlinux.org>"
+iso_publisher="Arch Linux <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
-#airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
-#airootfs_image_tool_options=('-comp' 'xz')
-airootfs_image_tool_options=('-comp' 'zstd')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
   ["/root"]="0:0:750"
