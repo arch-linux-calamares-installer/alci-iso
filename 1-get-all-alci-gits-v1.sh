ca80701721e6fa87114919089a1ac069e6dde4b2
Erik Dubois <erik.dubois@gmail.com>
2021-07-14 08:33:42 +0200

    get all githubs

---
+++ 1-get-all-alci-gits-v1.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+##################################################################################################################
+# Written to be used on 64 bits computers
+# Author 	: 	Erik Dubois
+# Website 	: 	http://www.erikdubois.be
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "This gets all the existing githubs at once"
+echo "Fill the array with the original folders first"
+
+# use ls -d */ > list to get the list of the created githubs and copy/paste in
+
+directories=(
+alci-calamares-config/
+alci-calamares-config-btrfs/
+alci-calamares-config-dev/
+alci-calamares-config-lts/
+alci-calamares-config-pure/
+alci-calamares-config-zen/
+alci-dwm/
+alci-dwm-nemesis/
+alci-iso/
+alci-iso-dev/
+alci-iso-lts/
+alci-iso-pure/
+alci-iso-zen/
+alci-pkgbuild/
+alci_repo/
+nemesis-wallpapers/
+)
+
+count=0
+
+for name in "${directories[@]}"; do
+	count=$[count+1]
+	tput setaf 1;echo "Github "$count;tput sgr0;
+	# if there is no folder then make one
+	git clone https://github.com/arch-linux-calamares-installer/$name
+	echo "#################################################"
+	echo "################  "$(basename `pwd`)" done"
+	echo "#################################################"
+done
