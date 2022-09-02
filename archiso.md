7c3f62cc16aa7ce944f45b1174dfad361b72235f
arcolinuxz <arcolinuxinfo@gmail.com>
2022-09-02 09:24:36 +0200

    archiso

--- archiso.md
+++ archiso.md
@@ -9,7 +9,7 @@ https://www.archlinux.org/packages/extra/any/archiso/
 We are now using this version
 
 
-archiso-version=archiso 65-1
+archiso-version=archiso 66-1
 
 
 sudo pacman -Q archiso and you will know your version
--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -35,7 +35,7 @@ echo
 	#isoLabel='arcolinux-next-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 65-1"
+	archisoRequiredVersion="archiso 66-1"
 	buildFolder=$HOME"/alci-build"
 	outFolder=$HOME"/Alci-Iso-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
