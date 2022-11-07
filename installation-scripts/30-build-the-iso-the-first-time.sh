22d62c79dafbe54ad41b48a34e57b33acf4a02a8
arcolinuxz <arcolinuxinfo@gmail.com>
2022-11-07 16:03:28 +0100

    archiso 68

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -35,7 +35,7 @@ echo
 	#isoLabel='arcolinux-next-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 67-1"
+	archisoRequiredVersion="archiso 68-1"
 	buildFolder=$HOME"/alci-build"
 	outFolder=$HOME"/Alci-Iso-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
