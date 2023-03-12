f044a1542797f5da8bf3a1561d75e22d397c091d
arcolinuxz <arcolinuxinfo@gmail.com>
2023-03-12 11:49:31 +0100

    test

---
+++ setup-my-credentials.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+#set -e
+##################################################################################################################
+# Author    : Erik Dubois
+# Website   : https://www.erikdubois.be
+# Website   : https://www.alci.online
+# Website   : https://www.ariser.eu
+# Website   : https://www.arcolinux.info
+# Website   : https://www.arcolinux.com
+# Website   : https://www.arcolinuxd.com
+# Website   : https://www.arcolinuxb.com
+# Website   : https://www.arcolinuxiso.com
+# Website   : https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
+##################################################################################################################
+
+echo
+tput setaf 3
+echo "################################################################"
+echo "################### Start"
+echo "################################################################"
+tput sgr0
+echo
+
+# Problem solving commands
+
+# Read before using it.
+# https://www.atlassian.com/git/tutorials/undoing-changes/git-reset
+# git reset --hard orgin/master
+# ONLY if you are very sure and no coworkers are on your github.
+
+# Command that have helped in the past
+# Force git to overwrite local files on pull - no merge
+# git fetch all
+# git push --set-upstream origin master
+# git reset --hard orgin/master
+
+
+#setting up git
+#https://www.atlassian.com/git/tutorials/setting-up-a-repository/git-config
+#https://medium.com/clarusway/how-to-use-git-github-without-asking-for-authentication-always-passwordless-usage-of-private-git-8c32489bc2e9
+#https://blog.nillsf.com/index.php/2021/05/27/github-sso-using-password-protected-ssh-keys
+
+project=$(basename `pwd`)
+#githubdir=$(basename $(dirname "$PWD" | tr '[:upper:]' '[:lower:]'))
+echo "-----------------------------------------------------------------------------"
+echo "this is project https://github.com/Arch-Linux-Calamares-Installer/"$project
+echo "-----------------------------------------------------------------------------"
+
+git config --global pull.rebase false
+git config --global push.default simple
+git config --global user.name "arcolinuxz"
+git config --global user.email "arcolinuxinfo@gmail.com"
+sudo git config --system core.editor nano
+git remote set-url origin git@github.com:arcolinuxz/$project
+
+echo
+tput setaf 3
+echo "################################################################"
+echo "################### End"
+echo "################################################################"
+tput sgr0
+echo
