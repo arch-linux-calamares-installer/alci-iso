20610611dd9636663745f746369e3f9cd2ae79d0
arcolinuxz <arcolinuxinfo@gmail.com>
2023-10-24 11:01:11 +0200

    archiso version

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -25,7 +25,7 @@ echo "################################################################## "
 echo
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 72-1"
+	archisoRequiredVersion="archiso 73-1"
 	buildFolder=$HOME"/alci-build"
 	outFolder=$HOME"/Alci-Iso-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
